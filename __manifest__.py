b7966ca771e9f47c04c58a1c2528fd7bb7b537e0
Roman Shveda <rshveda@gmail.com>
2020-12-29 13:39:39 +0200

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Twenty theme',
+    'description': 'Theme to practice web design.',
+    'version': '1.0',
+    'author': 'Roman Shveda',
+
+    'data': [
+        'views/assets.xml',
+        'views/layout.xml',
+        'views/pages.xml',
+        'views/produkte_pages.xml',
+    ],
+    'category': 'Theme/Creative',
+    'depends': ['website', 'website_theme_install'],
+}
